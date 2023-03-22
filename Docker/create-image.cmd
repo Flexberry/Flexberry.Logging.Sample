83aff68997e811f38b7b3150a48906e5e04bd3eb
AntonIV8 <anton-iv87@yandex.ru>
2023-03-22 09:33:20 +0500

    Правки имени образа loki

--- Docker/create-image.cmd
+++ Docker/create-image.cmd
@@ -2,4 +2,4 @@ docker build --no-cache -f App/Dockerfile.App -t flexberrysamplelogging/app ../
 
 docker build --no-cache -f Postgres/Dockerfile.PostgreSql -t flexberrysamplelogging/postgre-sql ../SQL
 
-docker build --no-cache -f GrafanaLoki/Dockerfile.Loki -t flexberry/loki .
+docker build --no-cache -f GrafanaLoki/Dockerfile.Loki -t flexberrysamplelogging/loki .
